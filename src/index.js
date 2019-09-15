2dde56f42fb3f0990b53a8420f8e2b812fc38686
Marcos <marcos_ppm@live.com>
2019-09-15 20:48:01 -0300

    First commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-
-ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+  
+class LaundryApp extends React.Component {
+    render() {
+        return (
+        <div className="laundry">
+            {"LaundryApp"}
+        </div>
+        );
+    }
+}
+  
+// ========================================
+  
+ReactDOM.render(
+    <LaundryApp />,
+    document.getElementById('root')
+);
+  
