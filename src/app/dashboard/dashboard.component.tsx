ac915f90586de89bc139eed54f533094e30f2663
Marcos <marcos_ppm@live.com>
2019-09-22 21:45:18 -0300

    Starting Dashboard

--- src/app/dashboard/dashboard.component.tsx
+++ src/app/dashboard/dashboard.component.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Button from 'react-bootstrap/Button';
 import Image from 'react-bootstrap/Image';
 import Container from 'react-bootstrap/Container';
 import Col from 'react-bootstrap/Col';
 import { DashboardHeaderStyled, ButtonStyled } from './dashboard.component.style';
 import { ThreeDotsIcon } from '../../resources';
 import { H1 } from '../../components';
+import Row from 'react-bootstrap/Row';
 
 export class Dashboard extends React.Component {
   render() {
@@ -15,6 +15,11 @@ export class Dashboard extends React.Component {
         <DashboardHeaderStyled className='align-middle'>
           <DashboardHeader />
         </DashboardHeaderStyled>
+        <Row>
+          <Col>
+            <DashboardTabs />
+          </Col>
+        </Row>
       </Container>
     );
   }
@@ -33,6 +38,15 @@ export const DashboardHeader = () => (
     </Col>
     <Col md={{ offset: 5 }}>
       <H1>Laundry CS</H1>
+    </Col>
+  </>
+);
+
+
+export const DashboardTabs = () => (
+  <>
+    <Col>
+      <H1>Tabs</H1>
     </Col>
   </>
 );
