c3b01897bb6a1606de38ea27d4561d99279ffad0
Marcos <marcos_ppm@live.com>
2019-09-24 23:14:35 -0300

    Added @types/styled-components and custom styles

---
+++ src/app/dashboard/dashboard.component.style.tsx
@@ -0,0 +1,6 @@
+import styled from 'styled-components';
+
+export const NavbarStyle = styled.nav`
+  width: 100%;
+  background-color: #0000EE;
+`;
--- src/app/dashboard/dashboard.component.tsx
+++ src/app/dashboard/dashboard.component.tsx
@@ -8,11 +8,12 @@ import Image from 'react-bootstrap/Image';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import { NavbarStyle } from './dashboard.component.style';
 
 export class Dashboard extends React.Component {
   render() {
     return (
-      <Container>
+      <Container fluid={true}>
         <Row>
           <Col>
             <DashboardHeaderButtons />
@@ -24,18 +25,14 @@ export class Dashboard extends React.Component {
 }
 
 const DashboardHeaderButtons = () => (
-<<<<<<< HEAD
-  <Navbar bg="primary" variant="dark" style={{marginLeft: 20}}>
-=======
-  <Navbar bg="primary" variant="dark">
->>>>>>> Started title bar
+  <NavbarStyle>
     <Button variant="outline-light">
       <Image
         src='https://facebook.github.io/react-native/img/tiny_logo.png'
-        width={10}
+        width={15}
         height={30}
       />
     </Button>
     <Navbar.Brand style={{marginLeft: 20}}>Laundry CS</Navbar.Brand>
-  </Navbar>
+  </NavbarStyle>
 );
