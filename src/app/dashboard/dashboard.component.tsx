c08932d8ddcd1c0c7294e010b3a3ba6e21eb985c
Marcos <marcos_ppm@live.com>
2019-09-24 22:29:44 -0300

    Added styled-components

---
+++ src/app/dashboard/dashboard.component.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Navbar from 'react-bootstrap/Navbar';
+
+import Button from 'react-bootstrap/Button';
+import Image from 'react-bootstrap/Image';
+
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
+export class Dashboard extends React.Component {
+  render() {
+    return (
+      <Container>
+        <Row>
+          <Col>
+            <DashboardHeaderButtons />
+          </Col>
+        </Row>
+      </Container>
+    );
+  }
+}
+
+const DashboardHeaderButtons = () => (
+  <Navbar bg="primary" variant="dark" style={{marginLeft: 20}}>
+    <Button variant="outline-light">
+      <Image
+        src='https://facebook.github.io/react-native/img/tiny_logo.png'
+        width={10}
+        height={30}
+      />
+    </Button>
+    <Navbar.Brand style={{marginLeft: 20}}>Laundry CS</Navbar.Brand>
+  </Navbar>
+);
